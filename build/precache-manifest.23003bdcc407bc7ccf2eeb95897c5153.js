0eac7d7f1701937d914c2b10ee5e8bdfda886452
Siddhant <jhasiddhant15@gmail.com>
2021-09-13 13:15:38 +0530

    minor fixes

---
+++ build/precache-manifest.23003bdcc407bc7ccf2eeb95897c5153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d462cd72bbdd9307f8d3d93a63fc8ce",
+    "url": "/ctf-challenge/index.html"
+  },
+  {
+    "revision": "1deca37a2dd5d0896550",
+    "url": "/ctf-challenge/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "1deca37a2dd5d0896550",
+    "url": "/ctf-challenge/static/js/2.0c505c78.chunk.js"
+  },
+  {
+    "revision": "384cab1b7cd6a44ee971731d66728117",
+    "url": "/ctf-challenge/static/js/2.0c505c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f7429f16f849b1195a",
+    "url": "/ctf-challenge/static/js/main.ff0d1ba7.chunk.js"
+  },
+  {
+    "revision": "0f7461eab519009ed67a",
+    "url": "/ctf-challenge/static/js/runtime-main.40a827ef.js"
+  }
+]);
